3f5623cf9069f640365bbab44a72331b9616d6ec
René Viering <rene.viering@gmail.com>
2016-11-26 16:58:31 +0100

    Remove unused uglify in dev config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import sourcemaps from 'rollup-plugin-sourcemaps';
