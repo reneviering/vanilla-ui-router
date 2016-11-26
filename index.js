c3a4a55cf407e2ed1d94ab6d51c5a0df2e56cb6d
René Viering <rene.viering@gmail.com>
2016-11-26 12:13:54 +0100

    Refactor

--- index.js
+++ index.js
@@ -26,59 +26,67 @@ export const createRouter = domEntryPoint => {
 		return paramValue;
 	};
 
-	const analyzeRoute = currentHash => {
-		if (currentHash === '') {
-			return {params: {}, isMatching: true, routeKey: ''};
-		}
+	const findMatchingRouteIdentifier = (currentHash, routeKeys) => {
+	  const splittedHash = currentHash.split('/');
+	  const firstHashToken = splittedHash[0];
 
-		const splittedHash = currentHash.split('/');
-		const first = splittedHash[0];
-		return Array.from(routes.keys())
-			.filter(routeKey => {
-				return routeKey.startsWith(first);
-			})
-			.reduce((acc, curr) => {
-				const splittedRouteDef = curr.split('/');
+	  return routeKeys
+	    .filter(routeKey => {
+	      const splittedRouteKey = routeKey.split('/');
 
-				if (splittedRouteDef.length === splittedHash.length) {
-					const params = [];
-					const result = splittedRouteDef.map((routeDefItem, i) => {
-						if(routeDefItem == splittedHash[i]) {
-							return true;
-						}
+	      const staticRouteTokensAreEqual = splittedRouteKey
+	        .map((routeToken, i) => {
+	          if(routeToken.startsWith(':')) return true;
+	          return routeToken === splittedHash[i];
+	        })
+	        .reduce((countInvalid, currentValidationState) => {
+	          if (currentValidationState === false) {
+	            ++countInvalid;
+	          }
+	          return countInvalid;
+	        }, 0) === 0;
 
-						if (routeDefItem.startsWith(':')) {
-							let param = {};
-							param[routeDefItem.substr(1, routeDefItem.length)] = splittedHash[i];
-							params.push(param);
-						}
-						return routeDefItem;
-					});
+	      return routeKey.startsWith(firstHashToken) &&
+	             staticRouteTokensAreEqual &&
+	             splittedHash.length === splittedRouteKey.length;
+	    })[0];
+	};
 
-					const isMatching = result.filter(r => r === false).length === 0;
+	const extractRouteParams = (routeIdentifier, currentHash) => {
+	  const splittedHash = currentHash.split('/');
+	  const splittedRouteIdentifier = routeIdentifier.split('/');
 
-					if(isMatching) {
-						const paramsObject = params.reduce((acc, curr) => {
-							Object.keys(curr).forEach(key => {
-								acc[key] = parseRouteParamToCorrectType(curr[key]);
-							});
-							return acc;
-						}, {});
-						return {params: paramsObject, isMatching: true, routeKey: curr};
-					}
-				}
-			}, {params: [], isMatching: false, routeKey: null});
-	}
+	  return splittedRouteIdentifier
+	    .map((routeIdentifierToken, index) => {
+	      if (!routeIdentifierToken.startsWith(':')) return null;
+	        const routeParam = {};
+	        const key = routeIdentifierToken.substr(1, routeIdentifierToken.length - 1);
+	        routeParam[key] = splittedHash[index];
+	        return routeParam;
+	    })
+	    .filter(p => p !== null)
+	    .reduce((acc, curr) => {
+	      Object.keys(curr).forEach(key => {
+	        acc[key] = parseRouteParamToCorrectType(curr[key]);
+	      })
+	      return acc;
+	    }, {});
+	};
 
 	const handleRouting = () => {
 		const defaultRouteIdentifier = '*';
 		const currentHash = location.hash.slice(1);
 
-		const analyzedRoute = analyzeRoute(currentHash);
-		const routeHandler = routes.has(analyzedRoute.routeKey) ? routes.get(analyzedRoute.routeKey) : routes.get(defaultRouteIdentifier);
+		const maybeMatchingRouteIdentifier = findMatchingRouteIdentifier(currentHash, Array.from(routes.keys()));
+	  let routeParams;
+		if (maybeMatchingRouteIdentifier) {
+			routeParams = extractRouteParams(maybeMatchingRouteIdentifier, currentHash);
+		}
+
+		const routeHandler = routes.has(maybeMatchingRouteIdentifier) ? routes.get(maybeMatchingRouteIdentifier) : routes.get(defaultRouteIdentifier);
 
 		if (typeof routeHandler === 'function') {
-			routeHandler(domEntryPoint, analyzedRoute.params);
+			routeHandler(domEntryPoint, routeParams);
 		}
 	};
 
